53bf219eb0a060766d74e9f3f1fd5494e8b0cffd
noebernigaud <noe.bernigaud@etu.unice.fr>
2022-06-22 00:20:46 +0200

    heroku

--- src/indexServer.tsx
+++ src/indexServer.tsx
@@ -1,3 +1,15 @@
 export function main() {
-  setInterval(() => { console.log("hey!") }, 10000);
+  let ws = new WebSocket("wss://mmoactiongameserver.herokuapp.com/");
+
+  ws.onerror = () => {
+    console.log("error trying to connect to socket on wss://mmoactiongameserver.herokuapp.com/");
+
+  };
+
+
+  //we start our request process when the connection is established
+  ws.onopen = (e) => {
+    //Ask username to user and removes " and ' characters. If user fails to give a username, give them a random id
+    setInterval(() => { ws.send("keepalive") }, 5000);
+  };
 }
